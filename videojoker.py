bb1609f9fd1850f030b54e916d66d3e7c97f909b
Morgan Van Valkenburgh <47067292+morgan-vanv@users.noreply.github.com>
2025-07-18 20:26:26 -0400

    Add sync and help commands, improve logging
    Introduces an admin-only /sync command to sync the command tree and a /help command to list all available commands. Improves logging for command invocations and disables the restart_bot method due to potential issues. Also ensures environment variables are loaded at startup.

--- videojoker.py
+++ videojoker.py
@@ -24,10 +24,10 @@ class VideoJoker(discord.Client):
 
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
 
-    def restart_bot(self):
-        os.execv(sys.executable, ['python'] + sys.argv)
+    # this might be causing issue when restarting the plugin consistently. disabling for now
+    # def restart_bot(self):
+    #     os.execv(sys.executable, ['python'] + sys.argv)
 
     async def on_ready(self):
         logging.info(f"{self.user} is now running.")
@@ -46,9 +46,34 @@ bot = VideoJoker()
 # Commands exposed to users
 @bot.tree.command()
 async def ping(ctx):
+    logging.info('/ping command invoked')
     await ctx.response.send_message('pong')
 
+# Admin only command to sync the command tree
+@bot.tree.command(name='sync', description='Admin only - Syncs the command tree.')
+async def sync(interaction: discord.Interaction):
+    # note: adding new commands requires a client restart to show the new commands
+    logging.info(f"{interaction.user.name} has activated /sync")
+    await bot.tree.sync()
+    await interaction.response.send_message('Command tree synced.', delete_after=5)
+    logging.info('Command tree synced.')
 
+# List of all commands
+@bot.tree.command(name='help', description='Shows list of all commands')
+async def help(ctx):
+    logging.info('/help command invoked')
+    embed = discord.Embed(
+        title="List of all commands",
+        # color=discord.Colour.dark_gray, todo: figure this out later, not working atm
+    )
+    embed.add_field(name="/ping", value="Returns pong", inline=True)
+    embed.add_field(name="/ping", value="Returns pong", inline=False)
+    embed.add_field(name="/ping", value="Returns pong", inline=False)
+    embed.add_field(name="/ping", value="Returns pong", inline=False)
+    await ctx.response.send_message(embed=embed, ephemeral=False)
+
+
+# Main entry point
 if __name__ == "__main__":
     # set up logging
     logging.basicConfig(
@@ -62,4 +87,5 @@ if __name__ == "__main__":
     logging.getLogger("discord").setLevel(logging.WARNING)
 
     # running the bot
+    load_dotenv()
     asyncio.run(bot.start_bot())
