5aaadb33c6660f01c4b236b652b05d5d413b302b
morpheus <francescofava4@gmail.com>
2026-06-05 20:27:11 -0400

    bot displays 3 buttons representing three cards for user to click and sends message confirmation of card that was clicked

--- cogs/games.py
+++ cogs/games.py
@@ -8,6 +8,28 @@ from discord.ext.commands import Cog
 
 from core.constants import EIGHT_BALL_RESPONSES, RPS_CHOICES, RPS_WINNING_CONDITIONS
 
+class MonteView(discord.ui.View):
+    """Class for the Three Card Monte UI"""
+    CARD_LABEL = "🃏"
+
+    def __init__(self, winning_card: str, user_id: int):
+        super().__init__(timeout=30)    # times out automatically if user doesn't press a button
+        self.user_id = user_id
+        self.winning_card = winning_card
+
+    # Functions that create the buttons - I would like to customize the look of the buttons a bit more
+    @discord.ui.button(label=CARD_LABEL, style=discord.ButtonStyle.primary, custom_id="A")
+    async def card_a(self, button, interaction):
+        await button.response.send_message(f"You clicked card {button.custom_id}!")
+
+    @discord.ui.button(label=CARD_LABEL, style=discord.ButtonStyle.primary, custom_id="B")
+    async def card_b(self, button, interaction):
+        await button.response.send_message(f"You clicked card {button.custom_id}!")
+
+    @discord.ui.button(label=CARD_LABEL, style=discord.ButtonStyle.primary, custom_id="C")
+    async def card_c(self, button, interaction):
+        await button.response.send_message(f"You clicked card {button.custom_id}!")
+
 class Games(Cog, name="Games"):
     """A cog that provides simple game commands"""
     def __init__(self, bot):
@@ -56,6 +78,14 @@ class Games(Cog, name="Games"):
 
 
         await interaction.response.send_message(f"{interaction.user.name} chose **{choice}**. I chose **{bot_choice}**. {result}")
+
+    @app_commands.command(name="threecardmonte", description="Play a game of Three Card Monte with the User")
+    async def threecardmonte(self, interaction: discord.Interaction) -> None:
+        """Runs the Three Card Monte game with the User using buttons"""
+        card = random.choice(["A", "B", "C"])
+        view = MonteView(winning_card=card, user_id=interaction.user.id)
+
+        await interaction.response.send_message("Pick a card any card!", view=view)
 
 async def setup(bot):
     await bot.add_cog(Games(bot))
